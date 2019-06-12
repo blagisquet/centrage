93c485cefc86718b3fbba84f46b1320d03ed8f5e
blagisquet <b.lagisquet@gmail.com>
2019-06-12 16:08:18 +0200

    fixed order by name, hours etc

--- src/Components/RDV/RDV.js
+++ src/Components/RDV/RDV.js
@@ -30,26 +30,26 @@ function RDV() {
       <table className="table table-hover table-bordered">
         <thead>
           <tr>
-            <th scope="col">Nom du client</th>
+            <th onClick={() => filterRdv('lastname', [setFilterLastname, filterLastname])} scope="col">Nom du client</th>
             <th scope="col">Prénom du client</th>
             <th scope="col">Lien personne de confiance</th>
-            <th scope="col">Nom du proche</th>
+            <th onClick={() => filterRdv('lastname', [setFilterLastname, filterLastname])} scope="col">Nom du proche</th>
             <th scope="col">Prénom du proche</th>
             <th scope="col">Téléphone</th>
-            <th scope="col">Horaires d&apos;appel</th>
+            <th onClick={() => filterRdv('hours', [setFilterHours, filterHours])} scope="col">Horaires d&apos;appel</th>
           </tr>
         </thead>
         <tbody>
-          {rdv.map((rdv, index) => {
+          {rdv.map((rendez, index) => {
             return (
               <tr key={[index]}>
-                <td onClick={() => filterRdv('lastname', [setFilterLastname, filterLastname])}>{rdv.lastName}</td>
-                <td>{rdv.name}</td>
-                <td>{rdv.relativePerson.relationship}</td>
-                <td onClick={() => filterRdv('lastname', [setFilterLastname, filterLastname])}>{rdv.relativePerson.lastName}</td>
-                <td>{rdv.relativePerson.name}</td>
-                <td>{rdv.relativePerson.phone}</td>
-                <td onClick={() => filterRdv('hours', [setFilterHours, filterHours])}>{rdv.hours}</td>
+                <td>{rendez.lastName}</td>
+                <td>{rendez.name}</td>
+                <td>{rendez.relativePerson.relationship}</td>
+                <td>{rendez.relativePerson.lastName}</td>
+                <td>{rendez.relativePerson.name}</td>
+                <td>{rendez.relativePerson.phone}</td>
+                <td>{rendez.hours}</td>
               </tr>
             );
           })}
