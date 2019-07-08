3053e724b60fee7fce8a7026e216969ed92f451d
blagisquet <b.lagisquet@gmail.com>
2019-07-08 17:36:33 +0200

    navbar working

--- src/App.jsx
+++ src/App.jsx
@@ -1,25 +1,10 @@
 import React from 'react';
 import './App.css';
-import { Route } from 'react-router-dom';
 import NavbarAdmin from './Components/NavbarAdmin/NavbarAdmin';
-import Category from './Components/Category/Category';
-import Homepage from './Components/Homepage/Homepage';
-import Login from './Components/Login/Login';
-import Questions from './Components/Questions/Questions';
-import Patients from './Components/Patients/Patients';
-import Patient from './Components/Patient/Patient';
-import RDV from './Components/RDV/RDV';
 
 const App = () => (
   <div>
     <NavbarAdmin />
-    <Route path="/login" component={Login} />
-    <Route path="/homepage" component={Homepage} />
-    <Route path="/category" component={Category} />
-    <Route path="/questions" component={Questions} />
-    <Route path="/clients" component={Patients} />
-    <Route exact path="/client/:id" component={Patient} />
-    <Route path="/RDV" component={RDV} />
   </div>
 );
 
